b3d030589dd5ae11e023f0691047f81ad6748ec5
Rohitvarma-ujeeni <Rohitvarma.ujeeni@gmail.com>
2025-07-05 22:03:12 +0530

    Delete tests/prod/def pytest_addoption(parser):     parser.addoption("--stage-url", action="store", default="http: directory

--- tests/prod/def pytest_addoption(parser):     parser.addoption("--stage-url", action="store", default="http:/conftest.py
+++
@@ -1,8 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption("--prod-url", action="store", default="http://localhost:8080", help="prod server base URL")
-
-import pytest
-
-@pytest.fixture
-def prod_url(request):
-    return request.config.getoption("--prod-url")
